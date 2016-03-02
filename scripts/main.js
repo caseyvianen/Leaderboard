804c436863d88d0fe035446d8b7699eba813c10c
caseyvianen <c.vianen@mondriaanict.nl>
2016-03-02 09:40:36 +0100

    Leaderboard Visual

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,34 @@
 angular.module("Leaderboard")
 .controller("mainLeaderboard", function($scope,leaderService){
+  var people = [
+   {name:'Casey',score:'500'},
+   {name:'Rob',score:'700'},
+   {name:'Roel',score:'605'},
+   {name:'Roy',score:'506'},
+   {name:'Alicia',score:'705'},
+   {name:'Roel',score:"400"},
+   {name:'Roel',score:"400"},
+   {name:'Roel',score:"400"},
+   {name:'Roel',score:"400"},
+   {name:'Roel',score:"400"},
+   {name:'Roel',score:"400"},
+   {name:'Roel',score:"400"},
+   {name:'Roel',score:"400"},
+   {name:'Roel',score:"400"},
+   {name:'Roel',score:"400"},
+   {name:'Roel',score:"400"},
+   {name:'Roel',score:"400"},
+   {name:'Roel',score:"400"},
+   {name:'Roel',score:"400"},
+   {name:'Roel',score:"400"},
+   {name:'Roel',score:"400"},
+   {name:'Roel',score:"400"},
+]
 
 leaderboardFunction = function()
 {
 
-  leaderService.leaderboardFunc(3);
+  leaderService.leaderboardFunc(3,people,40);
 
 }
 
--- services/service.js
+++ services/service.js
@@ -1,30 +1,6 @@
 angular.module("Leaderboard")
 .service("leaderService", function()
 {
-  var people = [
-   {name:'Casey',score:'500'},
-   {name:'Rob',score:'700'},
-   {name:'Roel',score:'605'},
-   {name:'Roy',score:'506'},
-   {name:'Alicia',score:'705'},
-   {name:'Roel',score:"400"},
-   {name:'Roel',score:"400"},
-   {name:'Roel',score:"400"},
-   {name:'Roel',score:"400"},
-   {name:'Roel',score:"400"},
-   {name:'Roel',score:"400"},
-   {name:'Roel',score:"400"},
-   {name:'Roel',score:"400"},
-   {name:'Roel',score:"400"},
-   {name:'Roel',score:"400"},
-   {name:'Roel',score:"400"},
-   {name:'Roel',score:"400"},
-   {name:'Roel',score:"400"},
-   {name:'Roel',score:"400"},
-   {name:'Roel',score:"400"},
-   {name:'Roel',score:"400"},
-   {name:'Roel',score:"400"},
-]
 
 
 
@@ -35,15 +11,55 @@ function SortByscore(x,y) {
 
 
 
-this.leaderboardFunc = function(startplek)
+
+this.leaderboardFunc = function(startplek,people,currentplace)
 {
-  people.sort(SortByscore);
   var plek= 20 *(startplek-2);
+  people.sort(SortByscore);
+
+  var body = document.querySelector('body');
+  var table= document.createElement('table');
+  var tr = document.createElement('tr');
+  var tablehead1= document.createElement('th');
+  var tablehead2= document.createElement('th');
+  var tablehead3= document.createElement('th');
+  tablehead1.innerHTML="Plek";
+  tablehead2.innerHTML="Naam";
+  tablehead3.innerHTML="Score";
+  tr.appendChild(tablehead1);
+  tr.appendChild(tablehead2);
+  tr.appendChild(tablehead3);
+  table.appendChild(tr);
+
+
     for(var i=0;i<=20;i++)
-    {
-      console.log(plek + people[i].name + ' ' + people[i].score + '<br>');
-      plek++;
+  {
+      var tr = document.createElement('tr');
+      var td1 = document.createElement('td');
+      var td2 = document.createElement('td');
+      var td3 = document.createElement('td');
+      if( plek == currentplace)
+      {
+        td1.style.background="Yellow"
+        td2.style.background="Yellow"
+        td3.style.background="Yellow"
+      }
+      td1.innerHTML=plek++;
+      td2.innerHTML=people[i].name;
+      td3.innerHTML=people[i].score;
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+      tr.appendChild(td3);
+      td1.style.border="1px solid black";
+      td2.style.border="1px solid black";
+      td3.style.border="1px solid black";
+    table.appendChild(tr);
+
     }
+    table.style.border="5px solid black";
+body.appendChild(table);
+
+
 
 
 }
